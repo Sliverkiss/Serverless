e1fa73f9f2b875f8440380cf483ccd07cecd99c0
Sliverkiss <1393579810@qq.com>
2024-07-07 00:00:40 +0800

    Update main.py

--- ddddocr/main.py
+++ ddddocr/main.py
@@ -1,103 +1,126 @@
-import os
-import time
-import ddddocr
-import uvicorn
+# encoding=utf-8
 import argparse
 import base64
-from fastapi import FastAPI, Form, File, UploadFile
+import json
 
-from pydantic import BaseModel
+import ddddocr
+from flask import Flask, request
 
 parser = argparse.ArgumentParser(description="使用ddddocr搭建的最简api服务")
-parser.add_argument("-p", "--port", type=int, default=6777)
+parser.add_argument("-p", "--port", type=int, default=6147)
+parser.add_argument("--ocr", action="store_true", help="开启ocr识别")
+parser.add_argument("--old", action="store_true", help="OCR是否启动旧模型")
+parser.add_argument("--det", action="store_true", help="开启目标检测")
+
 args = parser.parse_args()
 
-
-class Ocr():
-    ocr = ddddocr.DdddOcr()
-    det = ddddocr.DdddOcr(det=True)
-    slide = ddddocr.DdddOcr(det=False, ocr=False)
-
-    @staticmethod
-    def code_image(img: bytes):
-        return Ocr.ocr.classification(img)
-
-    @staticmethod
-    def det_image(img: bytes):
-        return Ocr.det.detection(img)
-
-    @staticmethod
-    def slide_image(target_img: bytes, background_img: bytes):
-        try:
-            return Ocr.slide.slide_comparison(target_img, background_img)
-        except Exception as e:
-            return Ocr.slide.slide_match(target_img, background_img)
+app = Flask(__name__)
 
 
-def ocr_img(type, img_bytes, background_img_bytes):
-    if type == 1:
-        return Ocr.code_image(img_bytes)
-    elif type == 2:
-        return Ocr.det_image(img_bytes)
-    elif type == 3:
-        return Ocr.slide_image(
-            img_bytes, background_img_bytes)
+class Server(object):
+    def __init__(self, ocr=True, det=False, old=False):
+        self.ocr_option = ocr
+        self.det_option = det
+        self.old_option = old
+        self.ocr = None
+        self.det = None
+        if self.ocr_option:
+            print("ocr模块开启")
+            if self.old_option:
+                print("使用OCR旧模型启动")
+                self.ocr = ddddocr.DdddOcr(old=True)
+            else:
+                print("使用OCR新模型启动，如需要使用旧模型，请额外添加参数  --old开启")
+                self.ocr = ddddocr.DdddOcr()
+        else:
+            print("ocr模块未开启，如需要使用，请使用参数  --ocr开启")
+        if self.det_option:
+            print("目标检测模块开启")
+            self.det = ddddocr.DdddOcr(det=True)
+        else:
+            print("目标检测模块未开启，如需要使用，请使用参数  --det开启")
+
+    def classification(self, img: bytes):
+        if self.ocr_option:
+            return self.ocr.classification(img)
+        else:
+            raise Exception("ocr模块未开启")
+
+    def detection(self, img: bytes):
+        if self.det_option:
+            return self.det.detection(img)
+        else:
+            raise Exception("目标检测模块模块未开启")
+
+    def slide(self, target_img: bytes, bg_img: bytes, algo_type: str):
+        dddd = self.ocr or self.det or ddddocr.DdddOcr(ocr=False)
+        if algo_type == 'match':
+            return dddd.slide_match(target_img, bg_img)
+        elif algo_type == 'compare':
+            return dddd.slide_comparison(target_img, bg_img)
+        else:
+            raise Exception(f"不支持的滑块算法类型: {algo_type}")
+
+server = Server(ocr=args.ocr, det=args.det, old=args.old)
+
+
+def get_img(request, img_type='file', img_name='image'):
+    if img_type == 'b64':
+        img = base64.b64decode(request.get_data()) # 
+        try: # json str of multiple images
+            dic = json.loads(img)
+            img = base64.b64decode(dic.get(img_name).encode())
+        except Exception as e: # just base64 of single image
+            pass
     else:
-        return None
+        img = request.files.get(img_name).read()
+    return img
 
 
-app = FastAPI()
+def set_ret(result, ret_type='text'):
+    if ret_type == 'json':
+        if isinstance(result, Exception):
+            return json.dumps({"status": 200, "result": "", "msg": str(result)})
+        else:
+            return json.dumps({"status": 200, "result": result, "msg": ""})
+        # return json.dumps({"succ": isinstance(result, str), "result": str(result)})
+    else:
+        if isinstance(result, Exception):
+            return ''
+        else:
+            return str(result).strip()
 
 
-class Item(BaseModel):
-    type: int = 1  # 1英数 2点选 3滑块
-    img: str
-    backgroundImg: str = None  # 滑块背景
-
-
-@app.post("/ocr")
-async def ocr_image(item: Item):
-    """ 识别Base64编码图片 """
+@app.route('/<opt>/<img_type>', methods=['POST'])
+@app.route('/<opt>/<img_type>/<ret_type>', methods=['POST'])
+def ocr(opt, img_type='file', ret_type='text'):
     try:
-        type = item.type
-        img_bytes = base64.b64decode(item.img, altchars=None, validate=False)
-        background_img_bytes = bytes()
-        if item.backgroundImg is not None:
-            background_img_bytes = base64.b64decode(
-                item.backgroundImg, altchars=None, validate=False)
-
-        t = time.perf_counter()
-
-        result = ocr_img(type, img_bytes, background_img_bytes)
-
-        return {'code': 1, 'result': result, 'consumeTime': int((time.perf_counter() - t)*1000), 'msg': 'success'}
+        img = get_img(request, img_type)
+        if opt == 'ocr':
+            result = server.classification(img)
+        elif opt == 'det':
+            result = server.detection(img)
+        else:
+            raise f"<opt={opt}> is invalid"
+        return set_ret(result, ret_type)
     except Exception as e:
-        return {'code': 0, 'result': None, 'msg': str(e).strip()}
+        return set_ret(e, ret_type)
 
-
-@app.post("/ocr/file")
-async def ocr_image_file(type: int = Form(1), img: UploadFile = File(None), backgroundImg: UploadFile = File(None)):
-    """ 识别文件上传图片 """
+@app.route('/slide/<algo_type>/<img_type>', methods=['POST'])
+@app.route('/slide/<algo_type>/<img_type>/<ret_type>', methods=['POST'])
+def slide(algo_type='compare', img_type='file', ret_type='text'):
     try:
-        img_bytes = await img.read()
-        background_img_bytes = bytes()
-        if backgroundImg is not None:
-            background_img_bytes = await backgroundImg.read()
-
-        t = time.perf_counter()
-        result = ocr_img(type, img_bytes, background_img_bytes)
-
-        return {'code': 1, 'result': result, 'consumeTime': int((time.perf_counter() - t)*1000), 'msg': 'success'}
+        target_img = get_img(request, img_type, 'target_img')
+        bg_img = get_img(request, img_type, 'bg_img')
+        result = server.slide(target_img, bg_img, algo_type)
+        return set_ret(result, ret_type)
     except Exception as e:
-        return {'code': 0, 'result': None, 'msg': str(e).strip()}
+        return set_ret(e, ret_type)
 
-
-@app.get("/ping")
+@app.route('/ping', methods=['GET'])
 def ping():
     return "pong"
 
 
 if __name__ == '__main__':
-    server_port = int(os.getenv("SERVER_PORT", args.port))
-    uvicorn.run(app='main:app', host="0.0.0.0",
-                port=server_port, reload=True)
+    app.run(host="0.0.0.0", port=args.port)
